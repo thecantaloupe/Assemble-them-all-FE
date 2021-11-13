cc4db267659a03fa43844408771285c101e8f574
justinrasler <85715506+justinrasler@users.noreply.github.com>
2021-11-13 15:21:50 -0500

    show page added

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,21 +1,65 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import Home from "../components/Main";
-import About from "./Index";
+import {useState} from "react";
 
-const Main = (props) => {
-    return(
-  <main>
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route path="/about">
-        <About />
-      </Route>
-    </Switch>
-  </main>
-    );
+function Show(props) {
+  //grab id from params
+  const id = props.match.params.id;
+  //save bookmark standalone variable
+  const people = props.bookmark
+  //find the bookmark to show
+  const bookmarked = bookmark.find((singleBookmark) => {
+    return singleBookmark._id === id;
+  });
+
+// state for our form
+const [editForm, setEditForm] = useState(bookmarked);
+
+ // handleChange function for form
+ const handleChange = (event) => {
+  setEditForm({ ...editForm, [event.target.name]: event.target.value });
 };
 
-export default Main;
+ // handleSubmit function for form submission
+ const handleSubmit = (event) => {
+  event.preventDefault()
+  props.updateBookmark(editForm, bookmarked._id)
+  // redirect people back to index
+  props.history.push("/")
+};
+ //remove function
+ const removeBookmark = () => {
+  props.deleteBookmark(bookmarked._id)
+  props.history.push("/")
+
+};
+
+return (
+  <div className="bookmarked">
+    <h1>{bookmark.title}</h1>
+    <h2>{bookmark.url}</h2>
+    <button onLCick={removeBookmark} id="delete">
+      DELETE
+    </button>
+
+    <form onSubmit={handleSubmit}>
+    <input
+        type="text"
+        value={editForm.title}
+        name="title"
+        placeholder="title"
+        onChange={handleChange}
+        />
+        <input
+        type="text"
+        value={editForm.url}
+        name="url"
+        placeholder="url"
+        onChange={handleChange}
+        />
+        <input type="submit" value="update the bookmark" />
+      </form> 
+  </div>
+);
+
+};
+
+export default Show;
